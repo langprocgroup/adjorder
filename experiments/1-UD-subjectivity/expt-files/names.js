8db89e3a8e6cbe2aa6c4a97cbc5b2d87e5c7ffd9
Greg Scontras <gscontras@gmail.com>
2019-11-06 10:03:29 -0800

    initialized subjectivity experiment

---
+++ experiments/1-UD-subjectivity/expt-files/names.js
@@ -0,0 +1,22 @@
+  var names = _.shuffle([
+    "Michael", "Christopher", "Matthew", "Joshua", "Jacob", "Nicholas",
+    "Andrew", "Daniel", "Tyler", "Joseph", "Brandon", "David", "James",
+    "Ryan", "John", "Zach", "Justin", "Will", "Anthony", "Robert", "Austin",
+    "Alexander", "Kyle", "Kevin", "Thomas", "Cody", "Jordan", "Eric", "Benjamin",
+    "Aaron", "Christian", "Samuel", "Dylan", "Steven", "Brian", "Jose",
+    "Timothy", "Nathan", "Adam", "Rick", "Patrick", "Charles", "Sean",
+    "Jason", "Cameron", "Jeremy", "Mark", "Juan", "Travis", "Jeff", "Ethan",
+    "Caleb", "Luis", "Jared", "Logan", "Hunter", "Trevor", "Evan", "Paul", "Kenneth",
+    "Connor", "Dustin", "Noah", "Carlos", "Devin", "Gabriel", "Ian", "Greg",
+    "Derek", "Corey", "Scott", "Bradley", "Jessica", "Ashley", "Emily", "Sarah",
+    "Samantha", "Amanda", "Brittany", "Elizabeth", "Megan", "Hannah", "Kayla",
+    "Lauren", "Stephanie", "Rachel", "Jennifer", "Nicole", "Alexis", "Victoria",
+    "Amber", "Alyssa", "Courtney", "Rebecca", "Danielle", "Jasmine", "Brianna",
+    "Katherine", "Alexandra", "Madison", "Morgan", "Melissa", "Michelle", "Kelsey",
+    "Chelsea", "Anna", "Kim", "Tiffany", "Olivia", "Mary", "Christina", "Allison",
+    "Abigail", "Heather", "Haley", "Maria", "Kaitlyn", "Laura", "Erin", "Andrea",
+    "Natalie", "Brooke", "Julia", "Emma", "Vanessa", "Erica", "Kelly", "Kristen",
+    "Marissa", "Amy", "Crystal", "Paige", "Cassandra", "Gabrielle", "Katie",
+    "Lindsey", "Destiny", "Kathryn", "Jacqueline", "Shannon", "Jenna", "Angela",
+    "Savannah", "Miranda"
+  ]);
